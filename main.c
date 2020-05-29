8b5eda6dd41e832d64c3f8c2c06dc05a648c0745
petru <petruignacio@gmail.com>
2020-05-29 18:07:43 -0300

    Insertar

--- main.c
+++ main.c
@@ -27,6 +27,30 @@ AVLtree itree_crear() {
 }
 
 
+int max(int a, int n) {
+    if(a > b)
+        return a;
+    return b;
+}
+
+AVLtree insertar(AVLtree arbol, Intervalo dato) {
+    if (arbol == NULL) {
+        //return crear nodo
+    }
+    if (dato.inicio > arbol->dato->inicio)
+        arbol->izq = insertar(arbol->izq, dato);    
+    if else (dato->inicio < arbol->dato->inicio)
+        arbol.der = insertar(arbol->der, dato);
+    else
+        return arbol;
+
+    arbol->altura = 1 + max(arbol->izq->altura, arbol->der->altura);
+    
+    int balance = obter_balance(arbol);
+    return balancear(arbol, balance);
+}
+
+
 
 int main() {
     
