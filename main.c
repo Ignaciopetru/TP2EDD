0957bd0709120fa867c8c764f52c5526109929ca
petru <petruignacio@gmail.com>
2020-05-29 19:27:48 -0300

    agregado balancear

--- main.c
+++ main.c
@@ -10,11 +10,11 @@ typedef struct _AVLnodo {
     Intervalo dato;
     int altura;
     double mayorFianl;
-    struct _AVLnodo *rigth;
-    struct _AVLnodo *left;
+    struct _AVLnodo *der;
+    struct _AVLnodo *izq;
 } AVLnodo;
 
-typedef AVLnodo *AVLtree;
+typedef AVLnodo *AVLTree;
 
 int obtenerAltura(AVLnodo *nodo) {
     if (nodo)
@@ -22,31 +22,83 @@ int obtenerAltura(AVLnodo *nodo) {
     return 0;
 }
 
-AVLtree itree_crear() {
+AVLTree itree_crear() {
     return NULL;
 }
 
 
-int max(int a, int n) {
+int max(int a, int b) {
     if(a > b)
         return a;
     return b;
 }
 
-AVLtree insertar(AVLtree arbol, Intervalo dato) {
+AVLTree rotar_derecha(AVLTree arbol) {
+    // Reservamos el izq del arbol
+    AVLTree copArbolIzq = arbol->izq;
+    // Realizamos la rotacion perse
+    arbol->izq = copArbolIzq->der;
+    copArbolIzq->der = arbol;
+    // Recalculamos altura
+    arbol->altura = max(obtenerAltura(arbol->izq), obtenerAltura(arbol->der)) + 1;
+    copArbolIzq->altura = max(obtenerAltura(copArbolIzq->izq), obtenerAltura(copArbolIzq->der)) + 1;
+    // Se retorna el nuevo primer nodo
+    return copArbolIzq;
+}
+
+AVLTree rotar_izquierda(AVLTree arbol) {
+    // Reservamos el der del arbol
+    AVLTree copArbolDer = arbol->der;
+    // Realizamos la rotacion perse
+    arbol->der = copArbolDer->izq;
+    copArbolDer->izq = arbol;
+    // Recalculamos altura
+    arbol->altura = max(obtenerAltura(arbol->izq), obtenerAltura(arbol->der)) + 1;
+    copArbolDer->altura = max(obtenerAltura(copArbolDer->izq), obtenerAltura(copArbolDer->der)) + 1;
+    // Se retorna el nuevo primer nodo
+    return copArbolDer;
+}
+
+AVLTree balancear(AVLTree arbol, int balance){
+    //Izquierda Izquierda
+    if(balance < -1 && obtener_balance(arbol->izq >= 0))
+        return rotar_derecha(arbol);
+    // Izquierda derecha
+    if(balance < -1 && obtener_balance(arbol->izq < 0)){
+        arbol->izq = rotar_izquierda(arbol->izq);
+        return rotar_derecha(arbol);
+    }
+    // Derecha derecha
+    if(balance > 1 && obtener_balance(arbol->izq <= 0))
+        return rotar_izquierda(arbol);
+    // Derecha izquieda
+    if(balance > 1 && obtener_balance(arbol->izq > 0)){
+        arbol->der = rotar_derecha(arbol->der);
+        return rotar_izquierda(arbol);
+    }
+    // Arbol balanceado
+    return arbol;
+}
+
+
+AVLTree insertar(AVLTree arbol, Intervalo dato) {
+    // Si llegamos a nodo vacio insertamos nuestro nodo nuevo
     if (arbol == NULL) {
         //return crear nodo
     }
+    // Buscamos la posicion que debe ocupar el nuevo nodo
+    // segun BST
     if (dato.inicio > arbol->dato->inicio)
         arbol->izq = insertar(arbol->izq, dato);    
     if else (dato->inicio < arbol->dato->inicio)
         arbol.der = insertar(arbol->der, dato);
+    // Caso en el que el nodo ya este en el arbol
     else
         return arbol;
-
+    // Recalculamos la altura de cada nuevo nodo
     arbol->altura = 1 + max(arbol->izq->altura, arbol->der->altura);
-    
-    int balance = obter_balance(arbol);
+    // Calculamos el valor de balance de el nodo y procedemos a balancear correctamente
+    int balance = obtener_balance(arbol);
     return balancear(arbol, balance);
 }
 
