f6212e038fa27ec479f43a44a88a9a241a73c1b4
petru <petruignacio@gmail.com>
2020-05-30 12:22:02 -0300

    problema en queue

--- queue.c
+++ queue.c
@@ -3,7 +3,7 @@
 #include "queue.h"
 #include "dlist.h"
 
-struct _Queue{
+typedef struct _Queue{
   DList primero;
   DList ultimo;
 };
@@ -15,7 +15,30 @@ Queue queue_new () {
     return cola;
 }
 
-void queue_agregar(Queue cola, void *dato) {
-    if(cola->primero = NULL)
-        
+int queue_isEmpty (Queue cola) {
+  return cola->primero == NULL;
 }
+
+void queue_agregar(Queue cola, void *dato) {
+  if (cola->primero = NULL) {
+    cola->primero = dnodo_agregar_inicio(NULL, dato);
+    cola->ultimo = cola->primero;
+  } else
+    cola->primero = dnodo_agregar_inicio(cola->primero, dato);
+}
+
+void* queue_sacar (Queue cola) {
+  if (cola->primero == NULL)
+    return NULL; 
+  void * dato = cola->ultimo->dato;
+  DList ultimo = cola->ultimo;
+  cola->ultimo = cola->ultimo->ant;
+  free(ultimo);
+  return dato;
+}
+
+void queue_destruir (Queue cola) {
+  dlist_destruir(cola->primero, NULL);
+  free(cola);
+}
+
--- queue.h
+++ queue.h
@@ -1,15 +1,17 @@
 #ifndef QUEUE_H_INLCUDED
 #define QUEUE_H_INLCUDED
 
-typedef struct _Queue* Queue;
+typedef struct _Queue QueueA;
+
+typedef QueueA* Queue;
 
 Queue queue_new ();
 
+int queue_isEmpty (Queue);
+
 void queue_agregar (Queue, void*);
 
-void queue_sacar (Queue);
-
-int queue_isEmpty (Queue);
+void * queue_sacar (Queue);
 
 void queue_destruir (Queue);
 
